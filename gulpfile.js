ad89b6407c703e48f53814246c1e047ff211f56c
Uwe Gerdes <entwicklung@uwegerdes.de>
2018-05-08 00:48:36 +0200

    removed comments

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,6 @@
  *
  * (c) Uwe Gerdes, entwicklung@uwegerdes.de
  *
- * ### Overview
- *
- * We are using [gulp](http://gulpjs.com/) as a build system. Gulp in
- * this project is responsible for a couple of things :
- *
- * 1. Compiles the project ( written in TypeScript ) to Javascript ;
- * 2. Helps during the development by watching for changes and
- *    compiles automatically.
- *
- * ### Structure
- *
- * Our gulp configuration starts in the root `./gulpfile.js`, which
- * loads all tasks in the directory `./gulp`.
- *
- * The gulp-task files itself are written according to JSDoc specs
- * to make generating the future documentation flawless.
- *
- * There is another special directory, called `./gulp/lib`, which
- * purpose is to store all non-gulptask files that have helpers
- * for the tasks (e.g. configuration options)
- *
- * ### External Configuration
- *
  * Gulp uses configuration variables stored in `./configuration.yaml`
  *
  * @name gulp
