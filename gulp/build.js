059cc0d2fe01fbbbb4b98b358e00bd3b27f88ac0
Uwe Gerdes <entwicklung@uwegerdes.de>
2018-05-10 14:44:14 +0200

    added styling for vcard list

--- gulp/build.js
+++ gulp/build.js
@@ -7,10 +7,10 @@ const gulp = require('gulp'),
   autoprefixer = require('gulp-autoprefixer'),
   jsdoc = require('gulp-jsdoc3'),
   less = require('gulp-less'),
-  lessChanged = require('gulp-less-changed'),
+  //lessChanged = require('gulp-less-changed'),
   notify = require('gulp-notify'),
   sequence = require('gulp-sequence'),
-  rename = require('rename'),
+  //rename = require('rename'),
   config = require('../lib/config'),
   loadTasks = require('./lib/load-tasks')
   ;
@@ -50,11 +50,11 @@ const tasks = {
    */
   'less': () => {
     return gulp.src(config.gulp.build.less.src)
-      .pipe(lessChanged({
-        getOutputFileName: (file) => { // jscs:ignore jsDoc
-          return rename(file, { dirname: config.gulp.build.less.dest, extname: '.css' });
-        }
-      }))
+      //.pipe(lessChanged({
+      //  getOutputFileName: (file) => { // jscs:ignore jsDoc
+      //    return rename(file, { dirname: config.gulp.build.less.dest, extname: '.css' });
+      //  }
+      //}))
       .pipe(less())
       .on('error', log.onError({ message:  'Error: <%= error.message %>', title: 'LESS Error' }))
       .pipe(autoprefixer('last 3 version', 'safari 5', 'ie 8', 'ie 9', 'ios 6', 'android 4'))
