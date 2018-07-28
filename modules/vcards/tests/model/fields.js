10ba831c1f396368be5c755e1797bef586a1791d
Uwe Gerdes <entwicklung@uwegerdes.de>
2018-07-28 23:31:00 +0200

    xStatus has default function

--- modules/vcards/tests/model/fields.js
+++ modules/vcards/tests/model/fields.js
@@ -25,4 +25,12 @@ describe('vcard fields', () => {
     assert.ok(model.fields.xTimestamp.default instanceof Function,
       'xTimestamp.default instanceof Function');
   });
+  it('should have xStatus with default function', () => {
+    assert.equal(Object.keys(model.fields.xStatus).length > 0, true, 'xStatus.length > 0');
+    const keylist = ['label', 'type', 'size', 'default'];
+    assert.deepEqual(Object.keys(model.fields.xStatus), keylist);
+    assert.ok(model.fields.xStatus.default instanceof Function,
+      'xStatus.default instanceof Function');
+    assert.equal(model.fields.xStatus.default('testvalue'), 'testvalue');
+  });
 });
