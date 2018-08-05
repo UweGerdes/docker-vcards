14ca11bea3f850b0d862184b307eb8b44dda6cdd
Uwe Gerdes <entwicklung@uwegerdes.de>
2018-08-05 22:04:30 +0200

    livereload changed files

--- gulp/server.js
+++ gulp/server.js
@@ -6,14 +6,26 @@
 'use strict';
 
 const gulp = require('gulp'),
+  changedInPlace = require('gulp-changed-in-place'),
   server = require('gulp-develop-server'),
   livereload = require('gulp-livereload'),
+  notify = require('gulp-notify'),
   sequence = require('gulp-sequence'),
   config = require('../lib/config'),
   ipv4addresses = require('../lib/ipv4addresses.js'),
   loadTasks = require('./lib/load-tasks'),
   log = require('../lib/log')
   ;
+
+/**
+ * log only to console, not GUI
+ *
+ * @param {pbject} options - setting options
+ * @param {function} callback - gulp callback
+ */
+const pipeLog = notify.withReporter((options, callback) => {
+  callback();
+});
 
 const tasks = {
   /**
@@ -45,8 +57,10 @@ const tasks = {
    */
   'livereload': () => {
     log.info('livereload triggered');
-    return gulp.src(config.gulp.watch.livereload[0])
+    return gulp.src(config.gulp.watch.livereload)
+      .pipe(changedInPlace({ howToDetermineDifference: 'modification-time' }))
       .pipe(livereload())
+      .pipe(pipeLog({ message: 'livereload: <%= file.path %>', title: 'Gulp livereload' }))
       ;
   },
   /**
@@ -86,7 +100,7 @@ const tasks = {
    * @namespace tasks
    */
   'livereload-start': () => {
-    livereload.listen({ port: config.server.livereloadPort, delay: 2000, quiet: true });
+    livereload.listen({ port: config.server.livereloadPort, delay: 2000, quiet: false });
     log.info('livereload listening on http://' +
       ipv4addresses.get()[0] + ':' + config.server.livereloadPort);
   }
