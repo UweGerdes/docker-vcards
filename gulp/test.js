5c13f915b524ff9dc110870d42b720942f961e94
Uwe Gerdes <entwicklung@uwegerdes.de>
2018-08-05 22:41:34 +0200

    use lib/files-promises

--- gulp/test.js
+++ gulp/test.js
@@ -5,17 +5,13 @@
  */
 'use strict';
 
-const fs = require('fs'),
-  glob = require('glob'),
-  gulp = require('gulp'),
+const gulp = require('gulp'),
   mocha = require('gulp-mocha'),
   sequence = require('gulp-sequence'),
   config = require('../lib/config'),
+  files = require('./lib/files-promises'),
   loadTasks = require('./lib/load-tasks')
   ;
-
-// execute only one test file if one has changed in recentTime, otherwise all
-const recentTime = 60; // * 60;
 
 const tasks = {
   /**
@@ -39,9 +35,9 @@ const tasks = {
    * @param {function} callback - gulp callback
    */
   'test-vcards': [['jshint'], (callback) => {
-      Promise.all(config.gulp.tests.vcards.map(getFilenames))
+      Promise.all(config.gulp.tests.vcards.map(files.getFilenames))
       .then((filenames) => [].concat(...filenames)) // jscs:ignore jsDoc
-      .then(getRecentFiles)
+      .then(files.getRecentFiles)
       .then((filenames) => { // jscs:ignore jsDoc
         const self = gulp.src(filenames, { read: false })
         // `gulp-mocha` needs filepaths so you can't have any plugins before it
@@ -60,51 +56,6 @@ const tasks = {
     }
   ],
 };
-
-/**
- * get list of files for glob pattern
- *
- * @private
- * @param {function} path - patterns for paths
- */
-const getFilenames = (path) => {
-  return new Promise((resolve, reject) => { // jscs:ignore jsDoc
-    glob(path, (error, files) => { // jscs:ignore jsDoc
-      if (error) {
-        reject(error);
-      } else {
-        resolve(files);
-      }
-    });
-  });
-};
-
-/**
- * get newest file from glob list - synchronous
- *
- * @param {array} files - list with glob paths
- */
-function getRecentFiles(files) {
-  let newest = null;
-  let bestTime = 0;
-  for (let i = 0; i < files.length; i++) {
-    const fileTime = fs.statSync(files[i]).mtime.getTime();
-    if (fileTime > bestTime) {
-      newest = files[i];
-      bestTime = fileTime;
-    }
-  }
-  const now = new Date();
-  if (now.getTime() - bestTime < recentTime * 1000) {
-    return new Promise((resolve) => { // jscs:ignore jsDoc
-      resolve([newest]);
-    });
-  } else {
-    return new Promise((resolve) => { // jscs:ignore jsDoc
-      resolve(files);
-    });
-  }
-}
 
 if (process.env.NODE_ENV == 'development') {
   loadTasks.importTasks(tasks);
